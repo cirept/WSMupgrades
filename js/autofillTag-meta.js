b4e8fd05c69a26c06d9c59dcf8fa4e81121afb6a
Eric Tanaka <eric.tanaka>
2017-09-07 11:05:52 -0700

    updated source files to match new method of font awesome
    - removed font aw css files and webfonts
    - made edits to the meta tag

--- js/autofillTag-meta.js
+++ js/autofillTag-meta.js
@@ -3,33 +3,12 @@
 // @namespace www.cobaltgroup.com
 // @run-at document-end
 // @include http://websites.cobalt.com/wsm/editSite.do*
+// @include http://websites.cobalt.com/wsm/cms.do*
 // @require https://code.jquery.com/jquery-3.2.1.js
 // @require https://cdn.jsdelivr.net/sortable/latest/Sortable.min.js
+// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/light.min.js
+// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/solid.min.js
+// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/fontawesome.min.js
 // @require https://rawgit.com/cirept/WSMupgrades/master/js/autofillTag.js
-// @grant GM_setValue
-// @grant GM_getValue
-// @grant GM_listValues
 // @author cire
 // ==/UserScript==
-
-(function () {
-    'use strict';
-    var $fonts = jQuery('<link>').attr({
-        'rel': 'stylesheet',
-        'id': 'awesome-light',
-        'href': 'https://cdn.rawgit.com/cirept/WSMupgrades/a7b46e8c58a100d9a116645ae61865d782988553/css/font-awesome-light.css',
-    });
-    var $fonts2 = jQuery('<link>').attr({
-        'rel': 'stylesheet',
-        'id': 'awesome-light',
-        'href': 'https://cdn.rawgit.com/cirept/WSMupgrades/master/css/font-awesome-solid.css',
-    });
-    var $fonts3 = jQuery('<link>').attr({
-        'rel': 'stylesheet',
-        'id': 'awesome-core',
-        'href': 'https://cdn.rawgit.com/cirept/WSMupgrades/master/css/font-awesome-core.css',
-    });
-    jQuery('head').append($fonts);
-    jQuery('head').append($fonts2);
-    jQuery('head').append($fonts3);
-})();
