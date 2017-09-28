20a6e02a2aeb59713d84d4a3ea8a9f856b781a92
Eric Tanaka <eric.tanaka>
2017-09-28 13:31:19 -0700

    updated meta to 0.0.7

--- js/autofillTag-meta.js
+++ js/autofillTag-meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name Autofill Tool - 0.0.5
+// @name Autofill Tool - 0.0.7
 // @namespace www.cobaltgroup.com
-// @version 0.0.5
+// @version 0.0.7
 // @run-at document-end
 // @include http://websites.cobalt.com/wsm/editSite.do*
 // @include http://websites.cobalt.com/wsm/cms.do*
@@ -10,6 +10,6 @@
 // @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/light.min.js
 // @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/solid.min.js
 // @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/fontawesome.min.js
-// @require https://rawgit.com/cirept/WSMupgrades/0.0.5/js/autofillTag.js
+// @require https://rawgit.com/cirept/WSMupgrades/0.0.7/js/autofillTag.js
 // @author cire.pt
 // ==/UserScript==
